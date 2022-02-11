8121d4b9a187f9d7d1d78330c52ccce9887780fc
Sayeed Najeeb <48534405+Sayeed-SCE@users.noreply.github.com>
2022-02-10 21:03:06 -0800

    completed the MVP object module challenge.

--- index.js
+++ index.js
@@ -172,9 +172,15 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
-} 
+function getLastReview(array) {
+
+
+array = `${array[7].name} gave the restaurant a ${array[7].rating} star review, and their feedback was: ${array[7].feedback}`;
+
+return array;
+}
+
+console.log (getLastReview(reviews));
 
 
 
